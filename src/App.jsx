8c097c34d796f131ec69360ab46eeeb3b452babf
shahnawazAlam3641 <shahnawazalam3641@gmail.com>
2025-05-14 19:38:20 +0530

    remove github activity

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import AboutMeSection from "./components/AboutMeSection";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import ProjectsSection from "./components/ProjectsSection";
+
 import Home from "./components/Home";
-import GitHubActivity from "./components/cactro/GitHubActivity";
-import RepoDetails from "./components/cactro/RepoDetails";
-import CreateIssue from "./components/cactro/CreateIssue";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/github" element={<GitHubActivity />} />
-      <Route path="/github/:repoName" element={<RepoDetails />} />
-      <Route path="/github/:repoName/issues" element={<CreateIssue />} />
     </Routes>
   );
 }
